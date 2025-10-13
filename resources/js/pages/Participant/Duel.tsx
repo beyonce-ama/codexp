e03ea87a83005fcbe1e27d33bde69a0992bdfa93
beyonce-ama <amabey222@gmail.com>
2025-10-13 20:11:22 +0800

    a

--- resources/js/pages/Participant/Duel.tsx
+++ resources/js/pages/Participant/Duel.tsx
@@ -2540,18 +2540,18 @@ const handleOpponentSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) =
                                                 <Flag className="h-4 w-4" />
                                                 <span>Surrender</span>
                                             </button>
-                                            
+{/*                                             
                                             {hasSubmitted && !lastSubmissionResult?.isCorrect && activeDuel.challenge.fixed_code && (
-                                                // <button
-                                                //     onClick={showCorrectAnswerHandler}
-                                                //     disabled={submitting}
-                                                //     className="flex items-center space-x-2 px-4 py-2 bg-gradient-to-r from-green-600 to-emerald-600 text-white rounded-lg hover:from-green-700 hover:to-emerald-700 disabled:opacity-50 transition-all duration-300 font-medium shadow-lg hover:scale-105"
-                                                //     onMouseEnter={() => audio.play('hover')}
-                                                // >
-                                                //     <BookOpen className="h-4 w-4" />
-                                                //     <span>FOR DEMO ONLY </span>
-                                                // </button>
-                                            )}
+                                                <button
+                                                    onClick={showCorrectAnswerHandler}
+                                                    disabled={submitting}
+                                                    className="flex items-center space-x-2 px-4 py-2 bg-gradient-to-r from-green-600 to-emerald-600 text-white rounded-lg hover:from-green-700 hover:to-emerald-700 disabled:opacity-50 transition-all duration-300 font-medium shadow-lg hover:scale-105"
+                                                    onMouseEnter={() => audio.play('hover')}
+                                                >
+                                                    <BookOpen className="h-4 w-4" />
+                                                    <span>FOR DEMO ONLY </span>
+                                                </button>
+                                            )} */}
                                         </div>
                                         
                                         <div className="flex items-center space-x-3">
